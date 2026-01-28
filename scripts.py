7b46b430312eeb93148c639f9cff38cfc549f7e3
Xuewen Yang <xuy001@ucsd.edu>
2026-01-27 17:05:36 -0800

    added file name and file extension

--- scripts.py
+++ scripts.py
@@ -9,6 +9,7 @@ mushroom_paths = pd.DataFrame({
     "path": [str(f) for f in files],
     "species": [f.parent.name for f in files],
     "class": [f.parent.parent.name for f in files],
+    "file_name": [f.name.split(".")[0] for f in files],
+    "file_ext": [f.name.split(".")[-1] for f in files]
 })
-mushroom_paths.to_csv("mushroom_paths.csv")
-
+mushroom_paths.to_csv("mushroom_paths.csv", index=False)
